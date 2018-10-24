38edc2a02ccb1ba2e8a39a8b8b561b3e7048d60c
Kien Nguyen <kiennt65@viettel.com.vn>
2018-10-24 15:26:39 +0700

    Add if-else example

---
+++ gobyexample/if-else/if-else.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	// A statement can precede conditionals;
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+}
